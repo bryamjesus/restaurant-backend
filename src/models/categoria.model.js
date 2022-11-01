60e87f5f0095f7f8009351d6ab61555bbfa45fc3
Bryam Jesus Talledo Garcia <btalledog@odybank.com.pe>
2022-10-31 21:08:56 -0500

    Add models

---
+++ src/models/categoria.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const categoriaSchema = new Schema({
+  nombre: { type: String, required: true },
+  estado: { type: String, default: 'A' } // A: activo || I: inactivo
+}, {
+  versionKey: false
+})
+
+module.exports = mongoose.model('categoria', categoriaSchema)
---
+++ src/models/usuario.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const usuarioSchema = new Schema({
+  tipo: { type: String, required: true }, // A:admin | C:cocina | M:mozo
+  nombres: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  estado: { type: String, default: "A" } // A:activo | I:inactivo
+}, {
+  versionKey: false
+});
+
+module.exports = mongoose.model("usuario", usuarioSchema);
