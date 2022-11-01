7bc1b445dbb42b061494ca4567931556cc43740c
Bryam Jesus Talledo Garcia <btalledog@odybank.com.pe>
2022-10-31 22:01:30 -0500

    Categoria

---
+++ src/controllers/categoria.controller.js
@@ -0,0 +1,62 @@
+const categoriaModel = require("../models/categoria.model");
+
+const controlador = {
+  async listar(req, res) {
+    try {
+      const result = await categoriaModel.find();
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+  async guardar(req, res) {
+    const { nombre } = req.body;
+    const dato = new categoriaModel();
+    dato.nombre = nombre;
+    try {
+      const result = await dato.save();
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+  async buscarPorId(req, res) {
+    try {
+      const { id } = req.params;
+      const result = await categoriaModel.findById(id);
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+  async editar(req, res) {
+    const { id } = req.params;
+    const { nombre, estado } = req.body;
+    const datos = {
+      nombre,
+      estado
+    };
+    try {
+      const result = await categoriaModel.findByIdAndUpdate(id, datos, { "new": true });
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+  async eliminar(req, res) {
+    try {
+      const { id } = req.params;
+      await categoriaModel.findByIdAndDelete(id);
+      res.sendStatus(200);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  }
+};
+
+module.exports = controlador;
