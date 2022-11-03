80e2f08ab82b2104b5500ac1d18e56b491e32630
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2022-11-02 22:32:41 -0500

    Update

--- src/controllers/cocina.controller.js
+++ src/controllers/cocina.controller.js
@@ -3,19 +3,26 @@ const pedidoModel = require("../models/pedido.model");
 const controlador = {
   async listar(req, res) {
     const result = await pedidoModel.find({ "detalle.estado": "P" });
-    res.json(result);
+    let datos = [];
+    result.forEach(pedido => {
+      const detalle = pedido.detalle;
+      const pendientes = detalle.filter(elem => elem.estado == "P");
+      datos = [...datos, ...pendientes];
+    });
+    res.json(datos);
   },
   async editar(req, res) {
     const { id } = req.params;
     try {
       const result = await pedidoModel.findOneAndUpdate(
         { "detalle._id": id },
-        { "$set": { "detalle.$[det].estado": "L" } },
+        { "$set": { "detalle.$[det].estado": "E" } },
         {
           arrayFilters: [{ "det._id": { $eq: id } }],
           "new": true
         }
       );
+      res.json(result);
     } catch (error) {
       console.log(error);
       res.sendStatus(500);
