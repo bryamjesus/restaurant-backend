41ae2a51115dce44bd41b2301f80892619cc77d8
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2022-11-02 19:53:14 -0500

    Add endpoint pedido

---
+++ src/routes/pedido.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const controlador = require("../controllers/pedido.controller");
+
+router.get("/listar", (req, res) => {
+  controlador.listar(req, res);
+});
+
+router.post("/guardar", (req, res) => {
+  controlador.guardar(req, res);
+});
+
+router.get("/detalle/:id", (req, res) => {
+  controlador.buscarPorId(req, res);
+});
+
+router.put("/editar/:id", (req, res) => {
+  controlador.editar(req, res);
+});
+
+module.exports = router;
