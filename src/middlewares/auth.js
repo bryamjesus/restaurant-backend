322f2b56932abba01d54f758250dd7159024013f
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2022-11-02 22:45:47 -0500

    Add validation

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,6 +4,9 @@ const { TOKEN_KEY } = require('../config')
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(" ")[1]
+  if(token === null){
+    return res.status(401).send("Token requerido")
+  }
 }
 
 module.exports = verifyToken
